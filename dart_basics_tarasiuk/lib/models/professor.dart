984cc79de737d07697036f059268331404306c61
Danil Tarasiuk <vodafonenous9388@gmail.com>
2025-09-23 18:17:41 +0200

    The professor.dart file was created and the tasks were completed

---
+++ dart_basics_tarasiuk/lib/models/professor.dart
@@ -0,0 +1,43 @@
+import 'person.dart';
+
+class Professor extends Person {
+  final String department;
+  final List<String> taughtCourses;
+  final double salary;
+
+  Professor({
+    required String id,
+    required String firstName,
+    required String lastName,
+    required DateTime birthDate,
+    required this.department,
+    required this.salary,
+    List<String>? taughtCourses,
+  }) : taughtCourses = taughtCourses ?? [],
+        super(
+          id: id, 
+          firstName: firstName, 
+          lastName: lastName, 
+          birthDate: birthDate
+        );
+
+  @override
+  String get role => 'Professor';
+
+  void assignCourse(String courseId) {
+    if (!taughtCourses.contains(courseId)) {
+      taughtCourses.add(courseId);
+    }
+  }
+
+  void removeCourse(String courseId) {
+    if (taughtCourses.contains(courseId)) {
+      taughtCourses.remove(courseId);
+    }
+  }
+
+  @override
+  String toString() {
+    return 'Professor(name: $fullName, department: $department, salary: $salary)';
+  }
+}
